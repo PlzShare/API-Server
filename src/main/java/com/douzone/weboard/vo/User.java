b1ee9c6309b4b979943d277cc91a708688c6fa63
onion237 <ssafy1121@gmail.com>
2021-11-29 18:57:29 +0900

    config file modified

--- src/main/java/com/douzone/weboard/vo/User.java
+++ src/main/java/com/douzone/weboard/vo/User.java
@@ -1,11 +1,15 @@
 package com.douzone.weboard.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class User {
 	private Long no;
