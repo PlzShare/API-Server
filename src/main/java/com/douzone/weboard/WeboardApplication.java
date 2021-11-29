801632063edc4e0e7140c261785247f91825bc8e
onion237 <ssafy1121@gmail.com>
2021-11-29 18:10:39 +0900

    initial commit

---
+++ src/main/java/com/douzone/weboard/WeboardApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.weboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douzone/weboard/WeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douzone.weboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
