6ff67b30ac77bb1b24baac86ca9bd7f6d2a81090
shyeonnnny <ktmdgus970807@gmail.com>
2021-12-09 20:13:02 +0900

    에쿠쿵!

--- src/main/java/com/douzone/weboard/controller/WorkspacesController.java
+++ src/main/java/com/douzone/weboard/controller/WorkspacesController.java
@@ -1,8 +1,6 @@
 package com.douzone.weboard.controller;
-
 import java.util.HashMap;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,45 +9,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.douzone.weboard.service.WorkspacesService;
 import com.douzone.weboard.util.ApiResult;
 import com.douzone.weboard.vo.Workspaces;
-
 @RestController // responsebody 다 붙어진 효과
-@RequestMapping("/workspaces/{userNo}")
+@RequestMapping("/workspaces")
 public class WorkspacesController {
-
 	@Autowired
 	private WorkspacesService workspacesService;
 	
 	// main
 	@GetMapping("")
-	public ResponseEntity<ApiResult> main(@PathVariable("userNo") Long userNo){
-		// 테스트로 유저넘버 1L 지정
-		// 병합개발 시 수정할 것(테스트용)
+	public ResponseEntity<ApiResult> main(
+			Workspaces workspace){
+		Long userNo = workspace.getUserNo();
 		List<Workspaces> list = workspacesService.findAll(userNo);
-		System.out.println(list);
 		return new ResponseEntity<ApiResult>(ApiResult.success(list), HttpStatus.OK); // 리턴 여러개로 정상동작 / 오류동작으로 분기
 	}
-
-	
-	// /insert
+	// insert
 	@PostMapping("")
 	public ResponseEntity<ApiResult> insert(
-			@PathVariable("userNo") Long userNo,
-			@RequestParam("name") String name){
-
-		System.out.println(userNo);
-		System.out.println(name);
-		// 워크스페이스 관리자가 워크스페이스 추가 가능
-		Workspaces workspace = Workspaces.builder()
-		        .userNo(userNo)
-		        .name(name)
-		        .build();
+			@RequestBody Workspaces workspace){
+		// json에 userNo, name 추가해서 보낼 것
 		workspacesService.insert(workspace);
 		return new ResponseEntity<ApiResult>(HttpStatus.OK);
 	}
@@ -57,12 +41,9 @@ public class WorkspacesController {
 	// update
 	@PutMapping("")
 	public ResponseEntity<ApiResult> update(
-			@PathVariable("userNo") Long userNo){
+			@RequestBody Workspaces workspace){
 		// 워크스페이스 관리자가 워크스페이스 수정 가능
-		Workspaces workspace = Workspaces.builder()
-				.name("편집성공?")
-		        .userNo(userNo)
-		        .build();
+		// json에 userNo, name 추가해서 보낼 것
 		workspacesService.update(workspace);
 		return new ResponseEntity<ApiResult>(HttpStatus.OK);
 	}
@@ -70,13 +51,16 @@ public class WorkspacesController {
 	// delete
 	@DeleteMapping("")
 	public ResponseEntity<ApiResult> delete(
-			@PathVariable("userNo") Long userNo){
+			@RequestBody Workspaces workspace){
 		// 워크스페이스 관리자가 워크스페이스 삭제 가능
-		Long test_workspaceNo = 2L;
-		workspacesService.delete(test_workspaceNo);
+		Long workspaceNo = workspace.getNo();
+		System.out.println(workspaceNo);
+		workspacesService.delete(workspaceNo);
 		return new ResponseEntity<ApiResult>(HttpStatus.OK);
 	}
 	
+	// 용신님 도와주세요
+	// search
 	@GetMapping("/search")
 	public ResponseEntity<ApiResult> search(
 			@PathVariable("userNo") Long userNo){
