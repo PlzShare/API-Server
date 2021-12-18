a1070ae518cc379723d3274c2cdf4efdb9cbc1c3
onion237 <ssafy1121@gmail.com>
2021-12-18 19:13:31 +0900

    auth

--- src/main/java/com/douzone/weboard/config/WebConfig.java
+++ src/main/java/com/douzone/weboard/config/WebConfig.java
@@ -1,47 +1,51 @@
 package com.douzone.weboard.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.douzone.weboard.interceptor.JwtInterceptor;
+import com.douzone.weboard.util.AuthUserArgumentResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
-	
+public class WebConfig implements WebMvcConfigurer {
+
 	@Autowired
 	private Environment env;
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
 		System.out.println(env.getProperty("fileupload.resourceMapping"));
-		System.out.println(env.getProperty("fileupload.fileUploadLocation")); 
-		
-		registry
-			.addResourceHandler(env.getProperty("fileupload.resourceMapping"))
-			.addResourceLocations(env.getProperty("fileupload.fileUploadLocation"));
-	}	
+		System.out.println(env.getProperty("fileupload.fileUploadLocation"));
+
+		registry.addResourceHandler(env.getProperty("fileupload.resourceMapping"))
+				.addResourceLocations(env.getProperty("fileupload.fileUploadLocation"));
+	}
+
 	@Autowired
 	private JwtInterceptor jwtInterceptor;
-	 
-	 private static final String[] EXCLUDE_PATHS = {
-	            "/error/**"
-	    };
 
-	    @Override
-	    public void addInterceptors(InterceptorRegistry registry) {
-	        registry.addInterceptor(jwtInterceptor)
-	                .addPathPatterns("/**")
-	                .excludePathPatterns("/users/login")
-	                .excludePathPatterns(EXCLUDE_PATHS);
-	    }
-	
-	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(jwtInterceptor)
+				.addPathPatterns("/**")
+				.excludePathPatterns("/users/login");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		// TODO Auto-generated method stub
+		resolvers.add(new AuthUserArgumentResolver());
+	}
+
 	
 }
--- src/main/java/com/douzone/weboard/interceptor/JwtInterceptor.java
+++ src/main/java/com/douzone/weboard/interceptor/JwtInterceptor.java
@@ -1,47 +1,60 @@
 package com.douzone.weboard.interceptor;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.douzone.weboard.vo.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
-public class JwtInterceptor implements HandlerInterceptor{
+public class JwtInterceptor implements HandlerInterceptor {
 	@Value("${authServer.url}")
 	private String authServerUrl;
-	
+	@Autowired
+	private ObjectMapper objectMapper;
 	private static final String HEADER_AUTH = "Authorization";
-	
+
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
-		
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
 		RestTemplate restTemplate = new RestTemplate();
-		
 		final String token = request.getHeader(HEADER_AUTH);
-		
 		HttpHeaders headers = new HttpHeaders();
-		
+
 		headers.set(HEADER_AUTH, token);
-		
 		HttpEntity requestEntity = new HttpEntity(headers);
 
-		System.out.println(token);
-		if(token != null) {
-			ResponseEntity<String> responseEntity = restTemplate.exchange(authServerUrl+"/validate", HttpMethod.GET, requestEntity, String.class );
-			
-			return true;
+		if (token != null) {
+			try {
+				ResponseEntity<String> responseEntity = restTemplate.exchange(authServerUrl + "/validate", HttpMethod.GET,
+						requestEntity, String.class);
+				User authUser = objectMapper.readValue(responseEntity.getBody(), User.class);
+				request.setAttribute("authUser", authUser);
+				return true;
+			}catch (RestClientException e) {
+				System.out.println(e);
+				response.setStatus(401);
+				return false;
+			}
+
 		}
+		
+		response.setStatus(401);
 		return false;
 	}
 }
